3f766c056c2429cab3a5890e47e9bd6af73b2808
miyan1221 <103588276+miyan1221@users.noreply.github.com>
2023-06-06 19:41:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import fetch from 'node-fetch';
 
 const port = parseInt(process.env.PORT || '8080', 10);
 const api_keys = JSON.parse(process.env.API_KEYS);
-const upstreamUrl = 'https://chatgpt-api.shn.hk/v1/chat/completions';
+const upstreamUrl = 'https://chatgpt-api.shn.hk/v1';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
